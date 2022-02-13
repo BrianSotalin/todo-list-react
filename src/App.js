8976a6186ee42ff50efdda7eb6215013942fece1
Brian Sotalin <dannysotalin@gmail.com>
2022-02-12 19:50:34 -0500

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [userName,setUserName]=useState("Sota");
+  const [taskItem,setTaskItem]=useState([
+    {name: 'Tarea-1',done:false},
+    {name: 'Tarea-2',done:false},
+    {name: 'Tarea-3',done:true},
+    {name: 'Tarea-4',done:false}
+  ]);
+  const taskTableRows=()=>{
+   return taskItem.map(task=>(
+      <tr>
+        <td>{task.name}</td>
+      </tr>
+   ))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1>sota</h1>
+      <table>
+        <tr>
+          <th class="desc">Description</th>
+          <th class="action">Done</th>
+        </tr>
+      </table>
+      <tbody>
+        {taskTableRows()}
+      </tbody>
     </div>
   );
 }
